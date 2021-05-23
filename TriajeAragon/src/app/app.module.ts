8f8dfb1399a568123143328cc52869b49501bd49
JavierFananasNaya <javierfananasnaya@gmail.com>
2021-05-23 10:58:17 +0200

    Primer commit, proyecto angular y api

---
+++ TriajeAragon/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {ButtonModule} from 'primeng/button';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
