3ba9941cd4947a887ebe8cc5bb65f214c83b0933
JavierFananasNaya <javierfananasnaya@gmail.com>
2021-06-12 11:21:11 +0200

    Añadido controlador pacientes y modelo

---
+++ api/webAPI/webAPI/Controllers/PatientController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webAPI.Models;
+
+namespace webAPI.Controllers
+{
+    public class PatientController : ApiController
+    {
+
+        public HttpResponseMessage Get() {
+            string query = @"
+                select id, name, surname, genre, bornDate, SSNumber, bloodType, adress, postalCode, phoneNumber
+                from dbo.pacientes
+            ";
+            DataTable table = new DataTable();
+
+            using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["TriajeAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd)) {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        public string Post(Patient pat) {
+            try
+            {
+                
+                string query = @"
+                    insert into dbo.pacientes VALUES ('" + pat.name + @"','" + pat.surname + @"','" + pat.genre + @"','" + pat.bornDate + @"','" + pat.SSNumber + @"','" + pat.bloodType + @"','" + pat.adress + @"'," + pat.postalCode + @"," + pat.phoneNumber + @")";
+
+                DataTable table = new DataTable();
+
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["TriajeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Added Succesfully!!";
+
+            }
+            catch (Exception)
+            {
+                return "Failed to Add!!";
+            }
+        }
+
+        public string Put(Patient pat)
+        {
+
+                string query = @"
+                    update dbo.pacientes set name = '" + pat.name + @"',surname = '" + pat.surname + @"', genre ='" + pat.genre + @"', bornDate = '" + pat.bornDate + @"', SSNumber = '" + pat.SSNumber + @"', bloodType = '" + pat.bloodType + @"', adress = '" + pat.adress + @"', postalCode = " + pat.postalCode + @", phoneNumber = " + pat.phoneNumber + @" WHERE id ="+ pat.id+@"
+                    ";
+            try
+            {
+
+                DataTable table = new DataTable();
+
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["TriajeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Updated Succesfully!!";
+
+            }
+            catch (Exception)
+            {
+                return "Failed to Update!!"+ query;
+            }
+        }
+
+        public string Delete(int patientId)
+        {
+
+            try
+            {
+                string query = @"
+                    delete from dbo.pacientes WHERE id =" + patientId + @"
+                    ";
+
+                DataTable table = new DataTable();
+
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["TriajeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Deleted Succesfully!!";
+
+            }
+            catch (Exception)
+            {
+                return "Failed to Delete!!";
+            }
+        }
+    }
+}
---
+++ api/webAPI/webAPI/Models/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webAPI.Models
+{
+    public class Patient
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string genre { get; set; }
+        public DateTime bornDate { get; set; }
+
+        public string SSNumber { get; set; }
+        public string bloodType { get; set; }
+        public string adress { get; set; }
+        public int postalCode { get; set; }
+        public int phoneNumber { get; set; }
+    }
+}
